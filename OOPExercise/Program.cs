e1fbf2941f9c8e2112c55fcaa7223efeac092a6e
admin <admin@HP50>
2018-08-22 15:54:35 +0300

    Added reading from text

--- OOPExercise/Program.cs
+++ OOPExercise/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace OOPExercise
 {
@@ -48,25 +49,63 @@ namespace OOPExercise
         static void Main(string[] args)
         {
 
-            Person jaan = new Person();
-            jaan.FirstName = "Jaan";
-            jaan.LastName = "Jaanson";
-            Console.WriteLine(jaan.FullName);
-            jaan.DateOfBirth = DateTime.Parse("12/09/1990");
-            Console.WriteLine(jaan.DateOfBirth);
-            Console.WriteLine(jaan.Age);
-            Console.WriteLine(jaan.FirstName);
+        
+           
+            string fileName = @"..\..\TextFile1.txt";
+            string[] file = File.ReadAllLines(fileName);
+          
+
+
+
+            foreach (string line in file)
+            {
+                
+                
+                string[] p = line.Split(',');
+                Person newPerson = new Person(p[0], p[1], DateTime.Parse(p[2]));
+            }
+
+
+
+            foreach (var person in Person.listOfPeople)
+            {
+                Console.WriteLine(person.ToString());
+            }
+
         }
     }
 
     class Person
     {
 
-        private string firstName;
-        private string lastName;
-        private string fullName;
-        private int age;
-        private DateTime dateOfBirth;
+        private string _FirstName;
+        private string _LastName;
+        private string _FullName;
+        private int _Age;
+        private DateTime _DateOfBirth;
+        private int _Nr; public int Nr => _Nr;
+
+        static Dictionary<int, Person> nimekiri = new Dictionary<int, Person>();
+
+        public static List<Person> listOfPeople = new List<Person> ();
+
+        public static int numberOfPeople = 1;
+
+        
+
+        public Person ( string firstName, string lastName, DateTime dateOfBirth) 
+            // this is the constructor
+            // we initialize the Person object and do some other stuff as well;
+        {
+            _FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth; // SET THE PROPERTY TO APPLY THE BUSINESS LOGIC!!
+            _FullName = firstName + " " + lastName;
+            _Age = DateTime.Now.Year - DateOfBirth.Year;
+            _Nr = numberOfPeople++;
+            
+            listOfPeople.Add(this);
+        }
 
 
         public string FirstName
@@ -74,43 +113,38 @@ namespace OOPExercise
 
             get
             {
-                return firstName;
+                return _FirstName;
             }
             set
             {
-                if (value.Substring(0, 1) == value.Substring(0, 1).ToUpper())
-                {
-                    firstName = value;
-                }
+                
+                _FirstName = value == "" ? value : value.Trim().Substring(0,1).ToUpper() + value.Trim().Substring(1).ToLower();
+                
             }
         }
 
+        
 
         public string LastName
         {
 
             get
             {
-                return lastName;
+                return _LastName;
             }
             set
             {
-                if (value.Substring(0, 1) == value.Substring(0, 1).ToUpper())
-                {
-                    lastName = value;
-                }
+                
+                _LastName = value == "" ? value : value.Trim().Substring(0, 1).ToUpper() + value.Trim().Substring(1).ToLower();
+                
             }
         }
 
         public string FullName { 
-            get {
-                if(firstName != null && lastName != null)
-                {
-                    return firstName + " " + lastName;
-                } else
-                {
-                    return null;
-                }
+            get
+            {
+
+                return _FullName;
 
             }
         }
@@ -118,12 +152,17 @@ namespace OOPExercise
 
             get
             {
-                
-                return DateTime.Now.Year - DateOfBirth.Year;
-               
+                return _Age;
             }
+
+            //set
+            //{
+                
+            //    _Age = DateTime.Now.Year - DateOfBirth.Year;
+               
+            //}
         }
-        public DateTime DateOfBirth { get => dateOfBirth;
+        public DateTime DateOfBirth { get => _DateOfBirth;
             set
             {
 
@@ -132,11 +171,11 @@ namespace OOPExercise
                 DateTime today = DateTime.Today;
                 if(value > today || value < today.AddYears(-80))
                 {
-                    Console.WriteLine((value - today));
-                    dateOfBirth = today;
+                    
+                    _DateOfBirth = today;
                 } else
                 {
-                    dateOfBirth = value;
+                    _DateOfBirth = value;
                 }
                     
                
@@ -144,7 +183,12 @@ namespace OOPExercise
 
         }
 
-        
+        public override string ToString()
+        {
+            return $"{Nr}. {FirstName}, {LastName}, {DateOfBirth}";
+        }
+
+
     }
 
 }
